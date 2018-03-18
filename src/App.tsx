5fea9d629c4770796bcc0c00fe963468979bd697
morsh <mor.shemesh@gmail.com>
2018-03-18 16:20:55 +0200

    adding style and theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import { Button } from 'react-md';
+import { NavigationDrawer } from 'react-md';
 
 import './App.css';
 
@@ -8,20 +10,29 @@ const Home = () => <h1>Home</h1>;
 const Admin = () => <h1>Admin</h1>;
 
 const App = () => (
-    <div className="App">
-      <header className="App-header">
-        <Router>
-          <div>
-            <NavBar />
-            <Route exact={true} path="/" component={Home} />
-            <Route path="/admin" component={Admin} />
-          </div>
-        </Router>
-      </header>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+      <NavigationDrawer
+        drawerTitle="react-md with CRA"
+        toolbarTitle="Welcome to react-md"
+      >
+        <div className="App">
+          <header className="App-header">
+            <Router>
+              <div>
+                <NavBar />
+                <Route exact={true} path="/" component={Home} />
+                <Route path="/admin" component={Admin} />
+              </div>
+            </Router>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+            <Button flat={true} primary={true} iconChildren="chat_bubble_outline">Chat</Button>
+            <Button flat={true}>Hello, World!</Button>
+            <Button raised={true} primary={true} swapTheming={true}>Hello</Button>
+            <Button raised={true} secondary={true} swapTheming={true}>World</Button>
+          </p>
+        </div>
+      </NavigationDrawer>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import * as WebFontLoader from 'webfontloader';
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+});
 
 ReactDOM.render(
   <App />,
