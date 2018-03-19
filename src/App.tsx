6e30c6b2a68121d53f52dad1e7f82627c735542c
morsh <mor.shemesh@gmail.com>
2018-03-19 12:46:47 +0200

    upgrade navigation building

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import * as H from 'history';
 import { NavigationDrawer } from 'react-md';
 import NavLink from './components/NavLink';
 
@@ -10,72 +11,77 @@ import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
 import Page3 from './pages/Page3';
 
-const navItems = [{
-  exact: true,
-  label: 'Home',
-  to: '/',
-  icon: 'home',
-}, {
-  label: 'Page 1',
-  to: '/page-1',
-  icon: 'bookmark',
-}, {
-  label: 'Page 2',
-  to: '/page-2',
-  icon: 'donut_large',
-}, {
-  label: 'Page 3',
-  to: '/page-3',
-  icon: 'flight_land',
-}];
+interface NavSettings {
+  exact?: boolean;
+  label: string;
+  to: string;
+  icon: string;
+  component: React.ComponentClass;
+}
+
+const DEFAULT_TITLE = 'Welcome';
+const navItems: NavSettings[] = [
+  {
+    exact: true,
+    label: 'Home',
+    to: '/',
+    icon: 'home',
+    component: Home
+  },
+  {
+    label: 'Page 1',
+    to: '/page-1',
+    icon: 'bookmark',
+    component: Page1
+  },
+  {
+    label: 'Page 2',
+    to: '/page-2',
+    icon: 'donut_large',
+    component: Page2
+  },
+  {
+    label: 'Page 3',
+    to: '/page-3',
+    icon: 'flight_land',
+    component: Page3
+  }
+];
 
 class App extends React.Component {
+
+  getLocationTitle(location: H.Location): string {
+    let currentPage = navItems.find(item => item.to === location.pathname);
+    return currentPage && currentPage.label || DEFAULT_TITLE;
+  }
 
   render() {
     return (
       <Route
         render={({ location }) => (
           <NavigationDrawer
-            drawerTitle="react-md with CRA"
-            toolbarTitle="Welcome to react-md"
+            drawerTitle="Site Navigation"
+            toolbarTitle={this.getLocationTitle(location)}
             navItems={navItems.map(props => <NavLink {...props} key={props.to} />)}
           >
             <Switch key={location.key}>
-              <Route exact={true} path="/" location={location} component={Home} />
-              <Route path="/page-1" location={location} component={Page1} />
-              <Route path="/page-2" location={location} component={Page2} />
-              <Route path="/page-3" location={location} component={Page3} />
+              {
+                navItems.map(props => (
+                  <Route
+                    key={props.to}
+                    exact={props.exact}
+                    path={props.to}
+                    location={location}
+                    component={props.component}
+                  />
+                ))
+              }
             </Switch>
           </NavigationDrawer>
         )}
       />
     );
   }
-  
-  // render () {
-  //   return (
-  //     <NavigationDrawer
-  //       drawerTitle="react-md with CRA"
-  //       toolbarTitle="Welcome to react-md"
-  //       navItems={navItems.map(props => <NavItemLink {...props} key={props.to} />)}
-  //     >
-  //       <div className="App">
-  //         <header className="App-header">
-  //           <NavBar />
-  //           <Route exact={true} path="/" component={Home} />
-  //           <Route path="/admin" component={Admin} />
-  //         </header>
-  //         <p className="App-intro">
-  //           To get started, edit <code>src/App.js</code> and save to reload.
-  //           <Button flat={true} primary={true} iconChildren="chat_bubble_outline">Chat</Button>
-  //           <Button flat={true}>Hello, World!</Button>
-  //           <Button raised={true} primary={true} swapTheming={true}>Hello</Button>
-  //           <Button raised={true} secondary={true} swapTheming={true}>World</Button>
-  //         </p>
-  //       </div>
-  //     </NavigationDrawer>
-  //   );
-  // }
 }
 
 export default App;
